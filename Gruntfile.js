fdacc27073eec1ae8cc432e69d7ac74ec6110d60
duergner <michael@pocketsunited.com>
2015-12-06 17:13:19 +0100

    * don't just listen on localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,7 +379,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          hostname: 'localhost',
+          hostname: '*',
           port: 4040
         }
       }
